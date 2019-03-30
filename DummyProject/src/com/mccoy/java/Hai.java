62216c0e7e5027dde1ad5bca27b600f0f146b6e0
Ravichandra K <kravichandra221@gmail.com>
2019-03-30 17:20:53 +0530

    Hai Class is Been Added and in the main Line 7 and Line 8 have been added

---
+++ DummyProject/src/com/mccoy/java/Hai.java
@@ -0,0 +1,7 @@
+package com.mccoy.java;
+
+public class Hai {
+	void display() {
+		System.out.println("Hello World");
+	}
+}
--- DummyProject/src/com/mccoy/java/Lab1.java
+++ DummyProject/src/com/mccoy/java/Lab1.java
@@ -4,5 +4,7 @@ public class Lab1 {
 	public static void main(String[] args) {
 		Hello h=new Hello();
 		h.show(10, 20);
+		Hai hai=new Hai();
+		hai.display();
 	}
 }
