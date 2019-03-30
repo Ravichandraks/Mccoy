0be67c89e381bd5303f7022f0edb27e43d36e471
Ravichandra K <kravichandra221@gmail.com>
2019-03-30 17:16:14 +0530

    First Program Commit

---
+++ DummyProject/src/com/mccoy/java/Hello.java
@@ -0,0 +1,8 @@
+package com.mccoy.java;
+
+public class Hello {
+	void show(int a,int b) {
+		int c=a+b;
+		System.out.println("Addition of 2 Numbers is:"+c);
+	}
+}
---
+++ DummyProject/src/com/mccoy/java/Lab1.java
@@ -0,0 +1,8 @@
+package com.mccoy.java;
+
+public class Lab1 {
+	public static void main(String[] args) {
+		Hello h=new Hello();
+		h.show(10, 20);
+	}
+}
